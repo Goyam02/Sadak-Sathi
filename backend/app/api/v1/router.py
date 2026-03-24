4d642d7a1fd4f22796e8b3a418639a906b216a05
Goyam02 <goyamjain02@gmail.com>
2026-03-24 14:26:19 +0530

    include routers

--- backend/app/api/v1/router.py
+++ backend/app/api/v1/router.py
@@ -1,3 +1,14 @@
 from fastapi import APIRouter
+import app.api.v1.auth as auth
+import app.api.v1.detection as detection
+import app.api.v1.alerts as alerts
+import app.api.v1.routes as routes
+import app.api.v1.hazards as hazards
 
 api_router = APIRouter()
+api_router.include_router(auth.router)
+api_router.include_router(detection.router)
+api_router.include_router(alerts.router)
+api_router.include_router(routes.router)
+api_router.include_router(hazards.router)
+
